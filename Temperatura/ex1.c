b2afa51f014e4f3a33a2fb1db97c52f1c941934a
EmersonVargas <38987145+EmersonVargas@users.noreply.github.com>
2018-05-07 23:57:39 -0300

    Update ex1.c

--- Temperatura/ex1.c
+++ Temperatura/ex1.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 int main(){
-	char temp[5];
+	char temp[50];
 	int i ,cont = 0;
 	float soma,media;
-	for (i=0; i<5; i++){
+	for (i=0; i<50; i++){
 		printf("Digite a temperatura: ");
 		scanf("%d",&temp[i]);
 		soma = soma + temp[i];	
 	}
-	media = soma/5;
-	for (i=0; i<5; i++){
+	media = soma/50;
+	for (i=0; i<50; i++){
 		if (temp[i] > media){
 			cont = cont + 1;
 		}
