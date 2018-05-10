fae85e1feb3c07a0a334fdc97ef9c20eac85bf2a
EmersonVargas <38987145+EmersonVargas@users.noreply.github.com>
2018-05-10 19:25:27 -0300

    Add files via upload

---
+++ Lab-Primo/todosp.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int eh_primo(int num){
+	int cont = 0, i, primo;	
+	for (i = 1; i <= num; i++) {
+   		if (num % i == 0) { 
+     		cont = cont + 1;
+    		}
+  	}
+	if (cont == 2){
+		primo = 1;	
+	}
+	else{
+		primo = 0;	
+	}
+	return primo;
+}
+int todos_os_primos(int num){
+	int i;
+	for (i = 1; i <= num; i++){
+		eh_primo(i);
+		if (eh_primo(i) == 1){
+			printf("%d\n",i);		
+		}	
+	}
+	return 0;	
+}
+
+int main(){
+	int max,i;
+	printf("Digite o numero maximo: ");
+	scanf("%d",&max);
+	todos_os_primos(max);	
+	return 0;
+}
