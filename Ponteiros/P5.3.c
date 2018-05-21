c3ab4e3e3a2c15b375f09ffada1a4b0af11d5c89
EmersonVargas <38987145+EmersonVargas@users.noreply.github.com>
2018-05-20 23:42:36 -0300

    Update P5.3.c

--- Ponteiros/P5.3.c
+++ Ponteiros/P5.3.c
@@ -6,42 +6,32 @@ e efetue as compras corretamente, sempre usando a
 conta com o maior saldo.
 **A cada compra o programa deve exibir os saldos 
 das duas contas.*/
-
 #include<stdio.h>
 
 void compra(int* conta, int valor){
-	*conta = *conta - valor;
+		printf("Valor da compra = %d\n",valor);
+		*conta = *conta - valor;
 }
 
 int main(){
 	int conta1, conta2, compras[] = {100, 50, 80, 30 ,20};
-	int i;
+	int i;	
 	printf("Digite o saldo da conta1: ");
 	scanf("%d",&conta1);
 	printf("Digite o saldo da conta2: ");
 	scanf("%d",&conta2);
-	if (conta1 >= conta2){
-		printf("Saldo inicial da conta1 = %d\n", conta1);
-		printf("Saldo inicial da conta2 = %d\n", conta2);		
-		printf("Compra efetuada na conta1. \n");
-			for ( i = 0; i < 5; i++){
-				printf("Valor da compra = %d\n",compras[i]);
-				compra(&conta1, compras[i]);
-				printf("Saldo da conta1 = %d\n",conta1);			
-			}		
-		printf("Saldo atualizado da conta1 = %d\n",conta1);
-		printf("Saldo atualizado da conta2 = %d\n",conta2);
-	}else{
-		printf("Saldo inicial da conta1 = %d\n", conta1);
-		printf("Saldo inicial da conta2 = %d\n", conta2);		
-		printf("Compra efetuada na conta2. \n");
-		for ( i = 0; i < 5; i++){
-				printf("Valor da compra = %d\n",compras[i]);
-				compra(&conta2, compras[i]);
-				printf("Saldo da conta2 = %d\n",conta2);			
-			}		
-		printf("Saldo atualizado da conta1 = %d\n",conta1);
-		printf("Saldo atualizado da conta2 = %d\n",conta2);
+	for ( i = 0; i < 5; i++){	
+		if (conta1 >= conta2){		
+			printf("Compra efetuada na conta1. \n");
+			compra(&conta1, compras[i]);		
+			printf("Saldo da conta1 = %d\n",conta1);
+			printf("Saldo da conta2 = %d\n",conta2);
+		}else{		
+			printf("Compra efetuada na conta2. \n");
+			compra(&conta2, compras[i]);
+			printf("Saldo da conta1 = %d\n",conta1);
+			printf("Saldo da conta2 = %d\n",conta2);
+		}
 	}
 	return 0;
 }
